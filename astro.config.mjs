52d3a7b7fabedf3b205583d80f6f6b04ae32ec01
Kai <orozobekov.kai@gmail.com>
2023-10-10 10:43:00 -0400

    refactor: canonical link added, sitemap generation added, robots.txt file added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  site: import.meta.env.SITE_URL,
+  integrations: [tailwind(), react(), sitemap()],
   image: {
     domains: ["https://res.cloudinary.com"],
   },
