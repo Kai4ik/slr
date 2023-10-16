35d28eee69f1323102165ab7550e24ebce0da600
Kai <orozobekov.kai@gmail.com>
2023-10-16 07:15:11 -0400

    refactor: minor fixes to meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: import.meta.env.SITE_URL,
+  site: "https://sexlivesreport.ca",
   integrations: [tailwind(), react(), sitemap()],
   image: {
     domains: ["https://res.cloudinary.com"],
